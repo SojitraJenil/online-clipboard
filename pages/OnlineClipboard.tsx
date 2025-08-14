76a4c31ca6e1a23781a6a0b2dca148764e061038
Jenil Sojitra <157600818+SojitraJenil@users.noreply.github.com>
2025-08-14 11:52:01 +0530

    Update OnlineClipboard.tsx

--- pages/OnlineClipboard.tsx
+++ pages/OnlineClipboard.tsx
@@ -221,17 +221,6 @@ export default function OnlineClipboard() {
 
 
 
-    <div style={{ width: "100%", height: "100vh" }}>
-      <iframe
-        src="https://example.com" // replace with allowed third-party URL
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "none",
-        }}
-        title="Third Party Content"
-      />
-    </div>
   
 
 
