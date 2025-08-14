45e7b0a52ec84b6f1146f8d2b842f96aefb7bb68
Jenil Sojitra <157600818+SojitraJenil@users.noreply.github.com>
2025-08-14 11:52:46 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,19 @@ export default function Home() {
     <div className="">
       <LoadingBarContainer>
         <OnlineClipboard />
+<br/>
+  <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://example.com" // replace with allowed third-party URL
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="Third Party Content"
+      />
+    </div>
+  
       </LoadingBarContainer>
     </div>
   );
