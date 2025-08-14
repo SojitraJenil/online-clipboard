44f87b829b9b7660cf67ea730cf7c631f4a3d5fd
Jenil Sojitra <157600818+SojitraJenil@users.noreply.github.com>
2025-08-14 12:04:33 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         style={{
           width: "100%",
           height: "100%",
-          border: "none",
+          border: "4px solid red",
         }}
         title="Third Party Content"
       />
